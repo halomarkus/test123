65f299d2b2934f003d912e22097c4a1b43a9e26d
halomarkus <32611356+halomarkus@users.noreply.github.com>
2018-03-19 23:02:32 +0100

    Antwort

--- Game restrictor/src/lmao/Launch.java
+++ Game restrictor/src/lmao/Launch.java
@@ -3,9 +3,11 @@ package lmao;
 public class Launch {
 
 	public static void main(String[] args) {
-
+String bruder1="Peter";
+String bruder2="Markus";
+		
 		System.out.println("okay, wer liest sich das durch?");
-
+		System.out.println(bruder1+" und "+bruder2);
 	}
 
 }
