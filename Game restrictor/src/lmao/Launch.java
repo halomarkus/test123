d3f51dc67df8afb44ec47e16e7ff6249093576dd
mimi- <mimi-@DESKTOP-LLGPHL6>
2018-03-19 22:31:12 +0100

    jjoooo

---
+++ Game restrictor/src/lmao/Launch.java
@@ -0,0 +1,11 @@
+package lmao;
+
+public class Launch {
+
+	public static void main(String[] args) {
+
+		System.out.println("okay, wer liest sich das durch?");
+
+	}
+
+}
---
+++ Game restrictor/src/lmao/Test1.java
@@ -0,0 +1,5 @@
+package lmao;
+
+public class Test1 {
+
+}
